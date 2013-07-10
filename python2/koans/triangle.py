e9bca7939c604808820543b99176fba838a9804a
Paulo Schneider <paulo.schneider@gmail.com>
2013-07-10 14:01:27 +0100

    Triangle with exceptions koans completed

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,13 +18,24 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-  if a == b == c:
+  if not is_a_triangle(a, b, c):
+    raise TriangleError
+  elif a == b == c:
     return "equilateral"
   elif a == b or b == c or a == c:
     return "isosceles"
   else:
     return "scalene"
 
+def is_a_triangle(a, b, c):
+  if a > b and a > c:
+    biggest, smallest_1, smallest_2 = a, b, c
+  elif b > c:
+    biggest, smallest_1, smallest_2 = b, a, c
+  else:
+    biggest, smallest_1, smallest_2 = c, a, b
+
+  return smallest_1 + smallest_2 > biggest
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(StandardError):
