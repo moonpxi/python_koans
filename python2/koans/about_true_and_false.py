b1ab9d73faa13a31d6e5e32b9142ee703221f128
Paulo Schneider <paulo.schneider@gmail.com>
2013-07-10 12:33:02 +0100

    About true and false koans are complete

--- python2/koans/about_true_and_false.py
+++ python2/koans/about_true_and_false.py
@@ -3,39 +3,42 @@
 
 from runner.koan import *
 
+is_true = 'true stuff'
+is_false = 'false stuff'
 
 class AboutTrueAndFalse(Koan):
+
     def truth_value(self, condition):
         if condition:
-            return 'true stuff'
+            return is_true
         else:
-            return 'false stuff'
+            return is_false
 
     def test_true_is_treated_as_true(self):
-        self.assertEqual(__, self.truth_value(True))
+        self.assertEqual(is_true, self.truth_value(True))
 
     def test_false_is_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(False))
+        self.assertEqual(is_false, self.truth_value(False))
 
     def test_none_is_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(None))
+        self.assertEqual(is_false, self.truth_value(None))
 
     def test_zero_is_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(0))
+        self.assertEqual(is_false, self.truth_value(0))
 
     def test_empty_collections_are_treated_as_false(self):
-        self.assertEqual(__, self.truth_value([]))
-        self.assertEqual(__, self.truth_value(()))
-        self.assertEqual(__, self.truth_value({}))
-        self.assertEqual(__, self.truth_value(set()))
+        self.assertEqual(is_false, self.truth_value([]))
+        self.assertEqual(is_false, self.truth_value(()))
+        self.assertEqual(is_false, self.truth_value({}))
+        self.assertEqual(is_false, self.truth_value(set()))
 
     def test_blank_strings_are_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(""))
+        self.assertEqual(is_false, self.truth_value(""))
 
     def test_everything_else_is_treated_as_true(self):
-        self.assertEqual(__, self.truth_value(1))
-        self.assertEqual(__, self.truth_value(1,))
+        self.assertEqual(is_true, self.truth_value(1))
+        self.assertEqual(is_true, self.truth_value(1,))
         self.assertEqual(
-            __,
+            is_true,
             self.truth_value("Python is named after Monty Python"))
-        self.assertEqual(__, self.truth_value(' '))
+        self.assertEqual(is_true, self.truth_value(' '))
